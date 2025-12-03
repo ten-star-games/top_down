09a201aa011cb3bd3bffc6d2eb798f9956ffca9d
anhyewon <metheduck92@gmail.com>
2025-12-04 04:18:08 +0900

    Effect Actor, BP_HealthPotion 추가

---
+++ Source/TD/Private/Actor/TDEffectActor.cpp
@@ -0,0 +1,46 @@
+﻿
+#include "Actor/TDEffectActor.h"
+
+#include "AbilitySystemComponent.h"
+#include "AbilitySystemInterface.h"
+#include "AbilitySystem/TDAttributeSet.h"
+#include "Components/SphereComponent.h"
+
+ATDEffectActor::ATDEffectActor()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>("Mesh");
+	SetRootComponent(Mesh);
+
+	Sphere = CreateDefaultSubobject<USphereComponent>("Sphere");
+	Sphere->SetupAttachment(GetRootComponent());
+}
+
+void ATDEffectActor::OnOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	//TODO: Change this to apply a Gameplay Effect. For now, using const_cast as a hack!
+	if (IAbilitySystemInterface* ASCInterface = Cast<IAbilitySystemInterface>(OtherActor))
+	{
+		const UTDAttributeSet* TDAttributeSet = Cast<UTDAttributeSet>(ASCInterface->GetAbilitySystemComponent()->GetAttributeSet(UTDAttributeSet::StaticClass()));
+
+		UTDAttributeSet* MutableTDAttributeSet = const_cast<UTDAttributeSet*>(TDAttributeSet);
+		MutableTDAttributeSet->SetHealth(TDAttributeSet->GetHealth() + 25.f);
+		Destroy();
+	}
+}
+
+void ATDEffectActor::EndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	
+}
+
+void ATDEffectActor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	Sphere->OnComponentBeginOverlap.AddDynamic(this, &ATDEffectActor::OnOverlap);
+	Sphere->OnComponentEndOverlap.AddDynamic(this, &ATDEffectActor::EndOverlap);
+}
---
+++ Source/TD/Public/Actor/TDEffectActor.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "TDEffectActor.generated.h"
+
+class USphereComponent;
+
+UCLASS()
+class TD_API ATDEffectActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	ATDEffectActor();
+
+	UFUNCTION()
+	virtual void OnOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+	virtual void EndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+protected:
+	virtual void BeginPlay() override;
+private:
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<USphereComponent> Sphere;
+
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<UStaticMeshComponent> Mesh;
+};
